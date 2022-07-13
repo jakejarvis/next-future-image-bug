38d65080993431e30657ea38882570bbc26e36bd
Jake Jarvis <jake@jarv.is>
2022-07-13 15:14:30 -0400

    add reproduction of `next/future/image` behavior when JS is disabled

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import("next").NextConfig} */
 const config = {
   reactStrictMode: true,
+  experimental: { images: { allowFutureImage: true } }
 }
 
 module.exports = config
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,22 @@
-/** Add your relevant code here for the issue to reproduce */
+import Image from "next/image";
+import FutureImage from "next/future/image";
+
+import pandaJpg from "../public/panda.jpg";
+
 export default function Home() {
-  return null
+  return (
+    <>
+      <h1>🐼 Please disable JavaScript from your dev tools!</h1>
+
+      <h2><code>next/future/image</code></h2>
+      <p>Two images will be produced here by a single <code>next/future/image</code> component. When running in <code>next dev</code>, the image on the left is blurred. In production, there are simply two duplicate images.</p>
+
+      <FutureImage src={pandaJpg} placeholder="blur" />
+
+      <h2><code>next/image</code></h2>
+      <p>Only one image appears here as expected, whether or not JS is enabled.</p>
+
+      <Image src={pandaJpg} placeholder="blur" />
+    </>
+  );
 }
